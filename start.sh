c85539ea40433e2fcc139da9262330002e8682ac
Nogura <resist_network@protonmail.com>
2018-12-22 08:35:21 -0500

    Bleh... automation and stuff.

--- start.sh
+++ start.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 if ! screen -list | grep -q "bot"; then
-	cd /storage/WA-Discord-Bot/
-#	killall ices;
-#	/storage/ices-start.sh;
+	cd /storage/discord-bot/
 	git stash;
 	git pull;
-        screen -LdmS bot node /storage/WA-Discord-Bot/bot.js
+        screen -LdmS bot node /storage/discord-bot/bot.js
 fi
-#if ! screen -list | grep -q "WA-Bot-Music"; then
-#	cd /storage/WA-Bot/
-#	git stash;
-#	git pull;
-#   screen -LdmS WA-Bot-Music node /storage/WA-Bot/music.js	
-#fi
