2976d49f796fe605f08472e6d5fc013ebed52141
system-oeprator <resist_network@protonmail.com>
2019-01-07 12:36:09 -0500

    Bleh... automation and stuff.

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+DISCORD=$(cat ~/.discord_token)
+MONGO=$(cat ~/.mongo)
+cd "$(dirname "$0")"
 if ! screen -list | grep -q "bot"; then
 	cd /storage/resist-discord-bot/
+	rm /storage/resist-discord-bot/config.json
+	sed "s#DISCORD_TOKEN#$DISCORD#g" /storage/resist-discord-bot/config.json.template > /storage/resist-discord-bot/config.json;
 	git stash;
 	git pull;
 	npm update;
