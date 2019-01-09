54c6a158ee3524d3b4a68f894bf3658a09a2d018
system-operator <resist_network@protonmail.com>
2019-01-09 15:44:55 -0600

    Bleh... automation and stuff.

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 DISCORD=$(cat ~/.discord_token)
+MYSQL_DATABASE=$(cat ~/.mysql_database)
+MYSQL_USER=$(cat ~/.mysql_user)
+MYSQL_PASSWORD=$(cat ~/.mysql_password)
+MYSQL_HOST=$(cat ~/.mysql_host)
 MONGO=$(cat ~/.mongo)
 cd "$(dirname "$0")"
 if ! screen -list | grep -q "bot"; then
 	cd /storage/resist-discord-bot/
 	rm /storage/resist-discord-bot/config.json
 	sed "s#DISCORD_TOKEN#$DISCORD#g" /storage/resist-discord-bot/config.json.template > /storage/resist-discord-bot/config.json;
+	sed -i "s/MYSQL_USER/$MYSQL_USER/g" /storage/resist-discord-bot/config.json;
+	sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /storage/resist-discord-bot/config.json;
+	sed -i "s/MYSQL_HOST/$MYSQL_HOST/g" /storage/resist-discord-bot/config.json;	
 	git stash;
 	git pull;
 	#npm update;
