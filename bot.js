7832d5087de059dd8f64f6b89ae5186543a04662
system-operator <resist_network@protonmail.com>
2019-01-15 18:20:39 -0500

    Bleh... automation and stuff.

--- bot.js
+++ bot.js
@@ -1341,7 +1341,7 @@ const commands = {
 									console.log(error)
 								  } else {
 									exec("rm /storage/listen.m3u");
-									exec("find /storage/resist-discord-bot/music | grep .mp3 > /storage/listen.m3u");
+									exec("find /storage/resist-discord-bot/assets/public/music | grep .mp3 > /storage/listen.m3u");
 									msg.channel.send(":white_check_mark:  `Added request from ` "+mentionCommandAuthor+" ` to Live Radio...` ```"+videoNamePretty+"\nDownloaded and encoded into MP3 (Audio)...\nAdded to Resist.Network Live Radio Queue...\nEnjoy!```Download it Here -> "+body.id+"\nListen Live in **#radio**, in Game or at -> https://Resist.Network/listen.mp3");	
 									//console.log(response.statusCode, body)
 								  }
@@ -1443,7 +1443,7 @@ const commands = {
 									console.log(error)
 								  } else {
 									exec("rm /storage/listen.m3u");
-									exec("find /storage/resist-discord-bot/music | grep .mp3 > /storage/listen.m3u");
+									exec("find /storage/resist-discord-bot/assets/public/music | grep .mp3 > /storage/listen.m3u");
 									msg.channel.send(":white_check_mark:  `Added request from in game player "+mentionCommandAuthor+" to Live Radio...` ```"+videoNamePretty+"\nDownloaded and encoded into MP3 (Audio)...\nAdded to Resist.Network Live Radio Queue...\nEnjoy!```Download it Here -> "+body.id+"\nListen Live in **#radio**, in Game or at -> https://Resist.Network/listen.mp3");	
 									//console.log(response.statusCode, body)
 								  }
