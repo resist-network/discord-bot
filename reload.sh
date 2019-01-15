eda4d981f3e9fc241a3ce47a5318070fbd2116db
system-operator <resist_network@protonmail.com>
2019-01-15 16:27:36 -0500

    Bleh... automation and stuff.

--- reload.sh
+++ reload.sh
@@ -2,9 +2,10 @@
 sleep 1;
 killall ices;
 /storage/ices-start.sh;
+cd /storage/resist-discord-bot;
 git stash;
 git pull;
-cd /storage/resist-discord-bot;
 #screen -S bot -X quit;
 screen -S bot-music -X quit;
-/storage/resist-discord-bot/start.sh && pkill -9 node
+/storage/resist-discord-bot/start.sh &
+pkill -9 node
