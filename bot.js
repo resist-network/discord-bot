db62626d16ac8d1a8e595b6f9514d2dbe743d004
system-operator <resist_network@protonmail.com>
2019-01-16 13:51:33 -0600

    Updates...

--- bot.js
+++ bot.js
@@ -1100,20 +1100,7 @@ const commands = {
 		var sys = require('util');
 		var exec = require('child_process').exec;
 		function puts(error, stdout, stderr) {
-			msg.channel.send({embed: {
-				color: 0xff8000,
-				author: {
-					name: bot_nickname+" - Self Update",
-					icon_url: bot_logo_square
-				},
-				//title: "Checking & Downloading Available Patches",
-				description: "```"+stdout+"```",
-			    timestamp: new Date(),
-			    footer: {
-			      text: info_copyright
-			    }
-				}
-			});
+			msg.channel.send("`"+stdout+"`");
 		}
 		exec("cd /storage/resist-discord-bot/; git pull; npm install;", puts);
  },'nslookup': (msg) => {
