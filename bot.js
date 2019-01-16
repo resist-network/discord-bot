ce01d02178375baad9572f28eba8b2ec2c2c1b7e
system-operator <resist_network@protonmail.com>
2019-01-16 02:55:16 -0500

    Bleh... automation and stuff.

--- bot.js
+++ bot.js
@@ -1255,8 +1255,8 @@ const commands = {
 			msg.channel.send('Search String: '+testRaw);
 			break;
 		case "wipe":
-			exec("rm -rf /storage/resist-discord-bot/assets/public/music/*.ogg");
-			exec("cp -rf /storage/resist-discord-bot/assets/public/music-orig/*.ogg /storage/resist-discord-bot/assets/public/music/.");
+			exec("rm -rf /storage/resist-discord-bot/assets/public/music/*.mp3");
+			exec("cp -rf /storage/resist-discord-bot/assets/public/music-orig/*.mp3 /storage/resist-discord-bot/assets/public/music/.");
 			exec("mv /storage/listen.m3u.orig /storage/listen.m3u");
  			msg.channel.send(":wastebasket:  `Wiping radio queue...`");
 			radioQueue(discord_channel_id_botspam);
@@ -1325,14 +1325,14 @@ const commands = {
 
 						ytdl.getInfo(videoUrl, function(err, info){
 							var videoName = info.title.replace('|','').replace(/[^a-zA-Z0-9-_]/g, '_').replace("_-_", "-").replace("__-__","-");
-							var videoWritableStream = fs.createWriteStream(tempDir + '/' + videoName + '.ogg'); 
+							var videoWritableStream = fs.createWriteStream(tempDir + '/' + videoName + '.mp3'); 
 							var stream = videoReadableStream.pipe(videoWritableStream);
 
 							function completeMessage() {
 								//
 								request.post('https://www.googleapis.com/urlshortener/v1/url?key='+api_google_shortener, {
 								  json: {
-									'longUrl': 'https://bot.Resist.Network/music/'+video.snippet.title+'.ogg'
+									'longUrl': 'https://bot.Resist.Network/music/'+video.snippet.title+'.mp3'
 								  }
 								}, function (error, response, body) {
 								  console.log(response);
@@ -1340,7 +1340,7 @@ const commands = {
 									console.log(error)
 								  } else {
 									exec("rm /storage/listen.m3u");
-									exec("find /storage/resist-discord-bot/assets/public/music | grep .ogg > /storage/listen.m3u");
+									exec("find /storage/resist-discord-bot/assets/public/music | grep .mp3 > /storage/listen.m3u");
 									msg.channel.send(":white_check_mark:  `Added request from ` "+mentionCommandAuthor+" ` to Live Radio...` ```"+videoNamePretty+"\nDownloaded and encoded into OGG (Audio)...\nAdded to Resist.Network Live Radio Queue...\nEnjoy!```Download it Here -> "+body.id+"\nListen Live in **#radio**, in Game or at -> https://Resist.Network/listen.mp3");	
 									//console.log(response.statusCode, body)
 								  }
@@ -1348,8 +1348,8 @@ const commands = {
 								//
 							}
 							msg.channel.send(playerQueryIntro, playerEmbed);
-							var tempFile = tempDir + '/' + videoName + '.ogg';
-							var mp3Path = musicDir + '/' + videoName + '.ogg';
+							var tempFile = tempDir + '/' + videoName + '.mp3';
+							var mp3Path = musicDir + '/' + videoName + '.mp3';
 							if (fs.existsSync(mp3Path)) {
 								completeMessage();
 							} else {
@@ -1362,7 +1362,7 @@ const commands = {
 										completeMessage();
 										//console.log('Done');
 									});					   
-									//move(tempDir + '/' + videoName + '.ogg', musicDir + '/' + videoName + '.ogg', completeMessage);
+									//move(tempDir + '/' + videoName + '.mp3', musicDir + '/' + videoName + '.mp3', completeMessage);
 								});    
 							}
 						});              
@@ -1428,21 +1428,21 @@ const commands = {
 
 						ytdl.getInfo(videoUrl, function(err, info){
 							var videoName = info.title.replace('|','').replace(/[^a-zA-Z0-9-_]/g, '_').replace("_-_", "-").replace("__-__","-");
-							var videoWritableStream = fs.createWriteStream(tempDir + '/' + videoName + '.ogg'); 
+							var videoWritableStream = fs.createWriteStream(tempDir + '/' + videoName + '.mp3'); 
 							var stream = videoReadableStream.pipe(videoWritableStream);
 
 							function completeMessage() {
 								//
 								request.post('https://www.googleapis.com/urlshortener/v1/url?key='+api_google_shortener, {
 								  json: {
-									'longUrl': 'https://radio.Resist.Network/music/'+video.snippet.title+'.ogg'
+									'longUrl': 'https://radio.Resist.Network/music/'+video.snippet.title+'.mp3'
 								  }
 								}, function (error, response, body) {
 								  if(error) {
 									console.log(error)
 								  } else {
 									exec("rm /storage/listen.m3u");
-									exec("find /storage/resist-discord-bot/assets/public/music | grep .ogg > /storage/listen.m3u");
+									exec("find /storage/resist-discord-bot/assets/public/music | grep .mp3 > /storage/listen.m3u");
 									msg.channel.send(":white_check_mark:  `Added request from in game player "+mentionCommandAuthor+" to Live Radio...` ```"+videoNamePretty+"\nDownloaded and encoded into OGG (Audio)...\nAdded to Resist.Network Live Radio Queue...\nEnjoy!```Download it Here -> "+body.id+"\nListen Live in **#radio**, in Game or at -> https://Resist.Network/listen.mp3");	
 									//console.log(response.statusCode, body)
 								  }
@@ -1450,8 +1450,8 @@ const commands = {
 								//
 							}
 							msg.channel.send(playerQueryIntro, playerEmbed);
-							var tempFile = tempDir + '/' + videoName + '.ogg';
-							var mp3Path = musicDir + '/' + videoName + '.ogg';
+							var tempFile = tempDir + '/' + videoName + '.mp3';
+							var mp3Path = musicDir + '/' + videoName + '.mp3';
 							if (fs.existsSync(mp3Path)) {
 								completeMessage();
 							} else {
