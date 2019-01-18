68f9cb40853d194f39f32761c330fdb6cf34285f
system-operator <resist_network@protonmail.com>
2019-01-18 07:31:48 -0600

    Bleh... automation and stuff.

--- bot.js
+++ bot.js
@@ -1086,8 +1086,8 @@ const commands = {
           },
           description: "`Player Name` "+mcUser+"\n`Rank` "+rank+
             "\n`Identification Number` "+uuidSecure+
-            "\n\n:date: `Join Date` "+joinDate+
-            "\n<:clock1:> `Time Played` "+timePlayed+
+            "\n\n:date: `Join Date` "+joinDate.replace('00:00:00 GMT-0500 (GMT-05:00)','')+
+            "\n:clock1: `Time Played` "+timePlayed+
             "\n<:Heart:532686774108160007> `Current Health` NA"+
             "\n<:credit:532687325101293579> `Credit Balance` "+bal,
         }}    
